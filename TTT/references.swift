560f465980615cb249cc67a718b7e750bedb8ee7
Anna Chiu <annachiu@Irenes-iMac.local>
2017-10-04 12:04:40 +0800

    added ab-pruning references

--- TTT/references.swift
+++ TTT/references.swift
@@ -5,10 +5,17 @@
  http://www.flyingmachinestudios.com/programming/minimax/
  
  How to create a tree structure in Swift: https://www.raywenderlich.com/138190/swift-algorithm-club-swift-tree-data-structure
- - didin't get up using
+ - didin't end up using
  
  Copied code for passing NSObject classes by VALUE: https://www.hackingwithswift.com/example-code/system/how-to-copy-objects-in-swift-using-copy
+ - didn't end up using
  
  Swift command line tutorial: https://www.raywenderlich.com/163134/command-line-programs-macos-tutorial-2
+ 
+ alpha - beta pruning:
+ http://inst.eecs.berkeley.edu/~cs61b/fa14/ta-materials/apps/ab_tree_practice/
+ https://www.youtube.com/watch?v=xBXHtz4Gbdo
+ 
+ Loop breaking in swift: https://stackoverflow.com/questions/24049629/how-to-break-outer-loops-from-inner-structures-that-respond-break-loops-switch
  
  */
